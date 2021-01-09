a935a05b8ba0a639a13af8bc26018ea4f6df0988
chengyijun <cyjmmy@foxmail.com>
2021-01-09 14:56:57 +0800

    mysql mongodb 数据库助手

--- handle_db/dbutils.py
+++ handle_db/dbutils.py
@@ -6,10 +6,11 @@
 @time: 2021/1/8 15:45
 @desc:
 """
+import pymongo
 import pymysql
 
 
-class DBUtils:
+class MysqlDBUtils:
     def __init__(self):
         # 1.连接
         self.conn = pymysql.connect(host='192.168.56.100', port=3306, user='root', password='root', database='abeltest',
@@ -34,21 +35,62 @@ class DBUtils:
         print('__del__() 销毁了数据库连接')
 
 
-def main():
-    db = DBUtils()
-    conn, cursor = db.get_conn_and_cursor()
+class MongoDBUtils:
+    def __init__(self, db: str, collect: str):
+        self.db = db
+        self.collect = collect
+        self.myclient = pymongo.MongoClient("mongodb://abel:abel@192.168.56.100:27017/")
+        # 获取数据库对象 如果不存在就创建
+        self.mydb = self.myclient[self.db]
+        # 获取集合对象 如果不存在就创建
+        self.mycol = self.mydb[self.collect]
 
+    def get_collection_obj(self):
+        return self.mycol
+
+    def close_conn(self):
+        try:
+            if self.myclient:
+                self.myclient.close()
+        except Exception as e:
+            print(e)
+
+    def __del__(self):
+        print('__del__() 关闭数据库连接')
+        self.close_conn()
+
+
+def main():
+    # mysql_test()
+    mongo_test()
+
+
+def mongo_test():
+    dbutils = MongoDBUtils('db1', 'coll1')
+    mycoll = dbutils.get_collection_obj()
+
+    # 新增数据
+    # mydict = {"name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com"}
+    # x = mycoll.insert_one(mydict)
+    # print('数据插入成功')
+    # print(x)
+
+    # 查询数据
+    for x in mycoll.find():
+        print(x)
+
+
+def mysql_test():
+    db = MysqlDBUtils()
+    conn, cursor = db.get_conn_and_cursor()
     # 注意%s需要加引号
     sql = "select * from person where name='%s'" % ('abel',)
     print(sql)
-
     # 3.执行sql语句
     cursor.execute(sql)
-
     result = cursor.execute(sql)  # 执行sql语句，返回sql查询成功的记录数目
     print(result)
     print(cursor.fetchall())
-
     # db.destroy_conn()
 
 
