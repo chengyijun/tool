3278cd89142d9dbc08daca940b0d18847ae1a0c0
chengyijun <cyjmmy@foxmail.com>
2021-01-12 18:19:23 +0800

    locust 补充

--- 性能测试locust/locust-01.py
+++ 性能测试locust/locust-01.py
@@ -6,6 +6,7 @@
 @time: 2020/12/29 8:31
 @desc:
 """
+
 from locust import HttpUser, TaskSet, task
 
 
@@ -28,6 +29,13 @@ class WebsiteUser(HttpUser):
     # task_set = WebsiteTasks
     # Usage of User.task_set is deprecated since version 1.0. Set the tasks attribute instead (tasks = [WebsiteTasks])
     tasks = [WebsiteTasks]
-    host = "http://debugtalk.com"
+    host = "http://127.0.0.1:5000"
     min_wait = 1000
     max_wait = 5000
+
+
+"""
+启动脚本：
+locust -f 性能测试locust/locust-01.py
+浏览器访问 http://localhost:8089
+"""
