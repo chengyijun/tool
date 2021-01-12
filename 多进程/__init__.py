efe15b84fec5a5c309b72c1174dbda718d331d0a
chengyijun <cyjmmy@foxmail.com>
2021-01-12 19:08:05 +0800

    结构调整

---
+++ 多进程/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+@author: chengyijun
+@contact: cyjmmy@foxmail.com
+@file: __init__.py.py
+@time: 2021/1/12 18:52
+@desc:
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
